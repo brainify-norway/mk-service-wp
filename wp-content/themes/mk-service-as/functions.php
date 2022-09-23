4f6625edd712cb43916893ad60287f440af39a73
Moisés Arão Rodrigues Magalhães <moises.arao@hotmail.com>
2022-09-23 09:00:55 +0200

    updating info

--- wp-content/themes/mk-service-as/functions.php
+++ wp-content/themes/mk-service-as/functions.php
@@ -42,11 +42,6 @@ if (function_exists('acf_add_options_page')) {
     'redirect'    => false
   ));
 
-  // acf_add_options_sub_page(array(
-  //   'page_title'   => 'Theme Header Settings',
-  //   'menu_title'  => 'Header',
-  //   'parent_slug'  => 'theme-general-settings',
-  // ));
 
   acf_add_options_sub_page(array(
     'page_title'   => 'Theme Footer Settings',
@@ -55,9 +50,9 @@ if (function_exists('acf_add_options_page')) {
   ));
 }
 
-add_action('admin_bar_menu', 'show_template');
-function show_template()
-{
-  global $template;
-  print_r($template);
-}
+// add_action('admin_bar_menu', 'show_template');
+// function show_template()
+// {
+//   global $template;
+//   print_r($template);
+// }
