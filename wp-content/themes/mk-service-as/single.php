9bf4107aaf85e7b7eac5a08f4611363d5f09a88c
Moisés Arão Rodrigues Magalhães <moises.arao@hotmail.com>
2022-09-23 14:01:03 +0200

    update of single.php

--- wp-content/themes/mk-service-as/single.php
+++ wp-content/themes/mk-service-as/single.php
@@ -2,21 +2,40 @@
 
 
 <div class="section wf-section">
-  <div class="content w-container pages">
+  <div class="content container-article">
     <h1 class="title">
       <?php while (have_posts()) : the_post();
         the_title();
       endwhile; ?>
     </h1>
-    <p class="content">
+
+    <div class="excerpt">
       <?php while (have_posts()) : the_post();
-        the_content();
+        the_excerpt();
       endwhile; ?>
-      <!-- Go to www.addthis.com/dashboard to customize your tools -->
-    <p><br><b>Del dette!</b>
-    <div class=“addthis_inline_share_toolbox”></div>
-    </p>
-    </p>
+    </div>
+
+    <div class="artikkel-bilde">
+      <img class="project-image" src="<?php the_post_thumbnail_url(); ?>">
+      <p class="artikkel-bilde-tekst"><?php get_the_author(); ?></p>
+      <p class="artikkel-bilde-tekst"><?php the_modified_time('F jS, Y'); ?></p>
+    </div>
+
+    <div class="artikkel-innhold-wrap">
+      <div class="artikkel-left">
+        <?php while (have_posts()) : the_post();
+          the_content();
+        endwhile; ?>
+      </div>
+      <div class="artikkel-right">
+        <div class="artikkel-faktaboks">
+          <h3 class="artikkel-fakta-heading">Del dette!</h3>
+          <!-- Go to www.addthis.com/dashboard to customize your tools -->
+          <p><br><b></b>
+          <div class=“addthis_inline_share_toolbox”></div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
