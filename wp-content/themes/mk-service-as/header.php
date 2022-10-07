b4a3065181f75c78c3716d1af8c4a89f05301820
Moisés Arão Rodrigues Magalhães <moises.arao@hotmail.com>
2022-10-07 14:00:44 +0200

    taking away one ""

--- wp-content/themes/mk-service-as/header.php
+++ wp-content/themes/mk-service-as/header.php
@@ -56,7 +56,7 @@
   <?php wp_head(); ?>
 
   <!-- Go to www.addthis.com/dashboard to customize your tools -->
-  <script type=“text/javascript” src=“//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-632c66645e3a99ce”></script>
+  <script type=“text/javascript” src=//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-632c66645e3a99ce></script>
 </head>
 
 <body <?php body_class(); ?>>
